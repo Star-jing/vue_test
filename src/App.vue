aab2e8602622ac3a17d63d0dc723f7a79260b696
Star-jing <ljh123789654@163.com>
2021-05-11 17:09:20 +0800

    新建了一个box组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+  <Box/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,11 +8,11 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Box from './components/Box.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,Box
   }
 }
 </script>
---
+++ src/components/Box.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.box{
+    width: 200px;
+    height: 200px;
+    background-color: rgb(223, 73, 73);
+}
+</style>
